ca885c11a876b4a11c4d592ee2d78ce8a7d64da1
most4f4 <mhasanalipourshahrab@myseneca.ca>
2025-08-19 23:31:09 -0400

    disable CSP temporarily

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https: blob: data:; object-src 'none';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
