245a9784c9ae0c3c5be60c2b917edec09360b640
most4f4 <mhasanalipourshahrab@myseneca.ca>
2025-08-19 22:55:49 -0400

    Update CSP to include the nonce pattern

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,8 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.amazoncognito.com https://*.auth.us-east-1.amazoncognito.com https://cognito-idp.us-east-1.amazonaws.com blob:",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://dnh6qh3zeqkj3.cloudfront.net https://*.amazoncognito.com https://*.auth.us-east-1.amazoncognito.com https://cognito-idp.us-east-1.amazonaws.com blob: 'nonce-*'",
+              "script-src-elem 'self' 'unsafe-inline' https://dnh6qh3zeqkj3.cloudfront.net https://*.amazoncognito.com blob:",
               "connect-src 'self' https://*.amazoncognito.com https://*.auth.us-east-1.amazoncognito.com https://cognito-idp.us-east-1.amazonaws.com https://fragments-lb-394296716.us-east-1.elb.amazonaws.com",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https:",
